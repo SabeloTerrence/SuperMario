4f71d77675cb2aad27f578f301febabbc54ea86f
sabelo <sabeloterrence@yahoo.com>
2016-04-05 11:29:35 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -16,12 +16,17 @@ int main(){
 	al_install_keyboard();
 	ALLEGRO_BITMAP *background = al_load_bitmap("WHu9Z.png");
 	ALLEGRO_BITMAP *player = al_load_bitmap("image1.png");
+	ALLEGRO_TIMER *timer = al_create_timer(0.5);
+	ALLEGRO_EVENT_QUEUE *event = al_create_event_queue();
+	al_register_event_source(event, al_get_keyboard_event_source());
+	al_register_event_source(event, al_get_timer_event_source(timer));
 	al_draw_bitmap(background, 0, 0, NULL);
-	al_draw_bitmap(player, 50, 50, ALLEGRO_FLIP_HORIZONTAL);
+	al_draw_bitmap(player, 0, 150, ALLEGRO_FLIP_HORIZONTAL);
 	if (!display)
 	{
 		al_show_native_message_box(NULL, NULL, NULL, "Couldn't initialise allegro", NULL, NULL);
 	}
+	al_flip_display();
 	al_rest(15.0);
 	al_destroy_bitmap(background);
 	al_destroy_bitmap(player);
